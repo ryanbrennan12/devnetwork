f456db7d396f8f7dec8497b9bdd50a3f4dd6e45a
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-11-21 13:35:16 -0800

    Register returning correct errors

--- routes/api/users.js
+++ routes/api/users.js
@@ -23,11 +23,18 @@ router.get('/test', (req, res) => {
 // @desc   Register User
 // @access Public
 router.post('/register', (req, res) => {
+  console.log('THIS IS THE BODY', req.body)
   const { errors, isValid } = validateRegisterInput(req.body)
+
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
 
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: 'Email already exists' });
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200',
