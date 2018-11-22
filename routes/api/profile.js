30f5c0d8695dc9da2379bea89220eb5265dd982a
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-11-21 18:16:55 -0800

    validating, populating

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+//Load Validation
+const validateProfileInput = require('../../validation/profile')
+
 //Load Profile Model
 const Profile = require('../../models/Profile');
 //Load User Model
@@ -25,6 +28,7 @@ router.get(
     const errors = {};
 
     Profile.findOne({ user: req.user.id })
+      .populate('user', ['name', 'avatar'])
       .then(profile => {
         if (!profile) {
           errors.noprofile = 'There is no profile for this user';
@@ -45,10 +49,17 @@ router.post(
   '/',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
+    const { errors, isValid }  = validateProfileInput(req.body);
+
+    //Check validation
+    if (!isValid) {
+      //Return any errors with 400 status
+      return res.status(400).json(errors)
+    }
     //Get fields
     const profileFields = {};
     profileFields.user = req.user.id;
-    if (req.body.hane) profdleileFields.handle = req.body.handle;
+    if (req.body.handle) profileFields.handle = req.body.handle;
     if (req.body.company) profileFields.company = req.body.company;
     if (req.body.website) profileFields.website = req.body.website;
     if (req.body.location) profileFields.location = req.body.location;
@@ -72,7 +83,7 @@ router.post(
     Profile.findOne({ user: req.user.id }).then(profile => {
       if (profile) {
         //Update
-        Profile.finfOneAndUpdate(
+        Profile.findOneAndUpdate(
           { user: req.user.id },
           { $set: profileFields },
           { new: true }
