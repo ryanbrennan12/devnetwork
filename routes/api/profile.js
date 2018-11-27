30f14d040cd658df5685da4ba392f2818429dc5c
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-11-26 18:41:32 -0800

    add '/handle/:handle' and '/user/:user_id'

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -42,6 +42,48 @@ router.get(
   }
 );
 
+// @route  POST GET/profile/handle/:handle
+// @desc   Get profile by handle
+// @access Public
+
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+  .populate('user', ['name', 'avatar'])
+  .then((profile) => {
+    if (!profile) {
+      errors.noprofile = 'There is no profile for this user'
+      res.status(404).json(errors);
+    }
+
+    res.json(profile)
+  })
+  .catch(err => res.status(404).json(err))
+});
+
+// @route  POST GET api/profile/user/:user_id
+// @desc   Get profile by user ID
+// @access Public
+
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+  .populate('user', ['name', 'avatar'])
+  .then((profile) => {
+    if (!profile) {
+      errors.noprofile = 'There is no profile for this user'
+      res.status(404).json(errors);
+    }
+
+    res.json(profile)
+  })
+  .catch(err => res.status(404).json(err))
+})
+
+
+
 // @route  POST api/profile
 // @desc   Create or edit user profile
 // @access Private
