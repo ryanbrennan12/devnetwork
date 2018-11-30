a0e5852537efaef4a90bb1fd0ee0428e744a6ee0
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-11-30 13:37:46 -0800

    add post validation/route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Post = require('../../models/Post');
+
+//Validation
+const validatePostInput = require('../../validation/post')
 
 
 // @route  GET api/posts/test
 // @desc   Tests posts route
 // @access Public
-
 router.get('/test', (req, res) => {
   res.json({message: "Posts Works!"})
+})
+
+// @route  POST api/posts
+// @desc   Create post
+// @access Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+
+  //Check Validation
+  if(!isValid) {
+    //if any errors, send 400 with errors object
+    return res.status(400).json(errors);
+  }
+
+  const newPost = new Post({
+    text: req.body.text,
+    name: req.body.name,
+    avatar:req.body.avatar,
+    user: req.body.id
+  });
+
+  newPost.save().then((post) => {
+    res.json(post)
+  })
 })
 
 module.exports = router;
