d017bdbd97215c288a95618d819516d38fa1c74e
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-12-02 15:30:52 -0800

    React-router implemented and functioning on first components

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
-
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Navbar from './components/layout/Navbar.jsx';
 import Footer from './components/layout/Footer.jsx';
 import Landing from './components/layout/Landing.jsx';
+import Register from './components/auth/Register.jsx';
+import Login from './components/auth/Login.jsx';
 
 import './App.css';
-
-
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-       <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
