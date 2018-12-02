a8af498fe4ddcf164f35a180e5f9e63815ae1bc3
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-12-02 11:42:03 -0800

    first components comiling w/no errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
+
 import React, { Component } from 'react';
-import './App.css';
+import Navbar from './components/layout/Navbar.jsx';
+import Footer from './components/layout/Footer.jsx'
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
        <h1>My React App</h1>
+       <Footer />
       </div>
     );
   }
